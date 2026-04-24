ca9884e24a50350f22329e8dd79a86f4b68eebf2
BaoLG2k7 <leb61187@gmail.com>
2026-04-24 23:02:58 +0700

    first commit

---
+++ Lesson1/code2.py
@@ -0,0 +1,4 @@
+name = input("Nhập tên:")
+age  = input("Nhập tuổi:")
+
+print(f"Xin chào {name}, bạn {age} tuổi")
---
+++ Lesson1/code3.py
@@ -0,0 +1,9 @@
+age = int(input('Nhập tuổi:'))
+
+if age >=18:
+    print('Chúc mừng bạn đã đủ 18 tuổi')
+    print('Bạn đủ điều kiện vào trong')
+else:
+    print('Bạn chưa đủ  18 tuổi')   
+    print('Bạn ko đủ điều kiện vào trong') 
+
