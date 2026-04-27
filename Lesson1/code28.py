edd5fbed74b9cb944c057ed95005b2c3f9e23586
LeGiaBao <leb61187@gmail.com>
2026-04-27 22:41:10 +0700

    Tìm 1 số lớn nhất

---
+++ Lesson1/code28.py
@@ -0,0 +1,11 @@
+a = int(input("Nhập số a:"))
+b = int(input("Nhập số b:"))
+c = int(input("Nhập số c:"))
+
+if a >= b and a >= c:
+    print("a lớn nhất")
+elif b >= a and b >= c:
+    print("b lớn nhất")
+else:
+    print("c lớn nhât")
+
