f937b687c3a1435b693308d7663819689dc0c87b
LeGiaBao <leb61187@gmail.com>
2026-04-27 22:45:36 +0700

    Tìm số lớn nhất(abc)

---
+++ Lesson1/code29.py
@@ -0,0 +1,13 @@
+a = int(input("Nhập số a:"))
+b = int(input("Nhập số b:"))
+c = int(input("Nhập số c:"))
+
+lon_nhat = max(a, b, c)
+
+if a == lon_nhat: 
+    print("a lớn nhất")
+if b == lon_nhat:
+    print("b lớn nhất")
+if c == lon_nhat:
+    print("c lớn nhất")
+    
